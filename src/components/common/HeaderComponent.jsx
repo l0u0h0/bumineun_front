b90f9ed2caeef5a8dccc1521740809eb8cc835af
l0u0h0 <l0u0h0@gmail.com>
2022-06-22 22:30:38 +0900

    header_update

--- src/components/common/HeaderComponent.jsx
+++ src/components/common/HeaderComponent.jsx
@@ -6,9 +6,15 @@ import search from "../../image/search.png";
 export default function Header() {
   return (
     <div className="header">
-      <img className="back" src={back} alt="backicon" />
-      <img className="logo" src={logo} alt="logo" />
-      <img className="search" src={search} alt="searchicon" />
+      <div className="back-area">
+        <img className="back" src={back} alt="backicon" />
+      </div>
+      <div className="logo-area">
+        <img className="logo" src={logo} alt="logo" />
+      </div>
+      <div className="search-area">
+        <img className="search" src={search} alt="searchicon" />
+      </div>
     </div>
   );
 }
