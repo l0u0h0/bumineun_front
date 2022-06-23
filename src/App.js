28a589aeab68798a162377bc12c0d9bc4895f739
l0u0h0 <l0u0h0@gmail.com>
2022-06-23 19:25:16 +0900

    tab_area_add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./css/Main.css";
 import "./css/Header.css";
 import "./css/Banner.css";
-import MainComponent from "./components/main/Maincomponent";
+import MainComponent from "./components/Maincomponent";
 
 function App() {
   return (
--- src/components/Maincomponent.jsx
+++ src/components/Maincomponent.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import Banner from "../common/BannerComponent";
-import Header from "../common/HeaderComponent";
+import Banner from "./common/BannerComponent";
+import Header from "./common/HeaderComponent";
 
 export default function MainComponent() {
   return (
     <div className="App-main">
       <Header />
       <div className="body" style={{ height: "760px" }}>
-        <div className="menu"></div>
+        <Body1 />
       </div>
       <div className="body" style={{ height: "1200px" }}></div>
       <Banner />
+    </div>
+  );
+}
+
+function Body1() {
+  return (
+    <div className="tab-navigation">
+      <hr />
+      <h3>Today's</h3>
+      <hr />
     </div>
   );
 }
