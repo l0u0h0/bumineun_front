f917877bab885325e3f484bccf150db3a538fedf
l0u0h0 <l0u0h0@gmail.com>
2022-06-23 20:40:03 +0900

    swiper_add

--- src/components/Maincomponent.jsx
+++ src/components/Maincomponent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Banner from "./common/BannerComponent";
 import Header from "./common/HeaderComponent";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 export default function MainComponent() {
   return (
@@ -20,7 +21,35 @@ function Body1() {
     <div className="tab-navigation">
       <hr />
       <h3>Today's</h3>
+      <div className="tab-title">
+        <button>실시간</button>
+        <button>단어별</button>
+        <button>통계</button>
+      </div>
+      <Swiper slidesPerView={1}>
+        <SwiperSlide>
+          <Tab1 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tab2 />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Tab3 />
+        </SwiperSlide>
+      </Swiper>
       <hr />
     </div>
   );
+}
+
+function Tab1() {
+  return <div className="Tab">실시간 탭</div>;
+}
+
+function Tab2() {
+  return <div className="Tab">단어별 탭</div>;
+}
+
+function Tab3() {
+  return <div className="Tab">통계 탭</div>;
 }
