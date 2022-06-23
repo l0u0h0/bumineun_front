006f5e5f03164b2088eedb3ac4f2f907dffa16e3
l0u0h0 <l0u0h0@gmail.com>
2022-06-23 22:28:39 +0900

    swiper-title-css_add

--- src/components/Maincomponent.jsx
+++ src/components/Maincomponent.jsx
@@ -23,54 +23,57 @@ function Body1() {
   return (
     <div className="tab-navigation">
       <hr />
-      <h3>Today's</h3>
-      <Swiper
-        className="tab-title"
-        slidesPerView={3}
-        onSwiper={setThumbsSwiper}
-        freeMode
-        watchSlidesProgress
-        modules={[FreeMode, Thumbs]}
-      >
-        <SwiperSlide>
-          <div className="tab1">실시간</div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="tab2">단어별</div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className="tab3">통계</div>
-        </SwiperSlide>
-      </Swiper>
-      <Swiper
-        className="tab-contents"
-        slidesPerView={1}
-        thumbs={{ swiper: thumbsSwiper }}
-        modules={[FreeMode, Thumbs]}
-      >
-        <SwiperSlide tabIndex={1}>
-          <Tab1 />
-        </SwiperSlide>
-        <SwiperSlide tabIndex={2}>
-          <Tab2 />
-        </SwiperSlide>
-        <SwiperSlide tabIndex={3}>
-          <Tab3 />
-        </SwiperSlide>
-      </Swiper>
+      <h2>Today's</h2>
+      <div className="swiper-area">
+        <Swiper
+          className="tab-title"
+          slidesPerView={3}
+          onSwiper={setThumbsSwiper}
+          freeMode
+          watchSlidesProgress
+          modules={[FreeMode, Thumbs]}
+        >
+          <SwiperSlide>
+            <div className="title">실시간</div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="title">단어별</div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="title">통계</div>
+          </SwiperSlide>
+        </Swiper>
+        <Swiper
+          className="tab-contents"
+          slidesPerView={1}
+          thumbs={{ swiper: thumbsSwiper }}
+          modules={[FreeMode, Thumbs]}
+        >
+          <SwiperSlide>
+            <Tab1 />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Tab2 />
+          </SwiperSlide>
+          <SwiperSlide>
+            <Tab3 />
+          </SwiperSlide>
+        </Swiper>
+      </div>
+
       <hr />
     </div>
   );
 }
 
 function Tab1() {
-  return <div className="Tab">실시간 탭</div>;
+  return <div className="contents">실시간 탭</div>;
 }
 
 function Tab2() {
-  return <div className="Tab">단어별 탭</div>;
+  return <div className="contents">단어별 탭</div>;
 }
 
 function Tab3() {
-  return <div className="Tab">통계 탭</div>;
+  return <div className="contents">통계 탭</div>;
 }
