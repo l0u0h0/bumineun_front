5c4ad5366be3150f2a0ca1cfc2bc0744ccca9793
l0u0h0 <l0u0h0@gmail.com>
2022-07-03 00:05:56 +0900

    search_add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import "./css/Main.scss";
 import "./css/Header.scss";
 import "./css/Banner.scss";
+import "./css/Search.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import MainComponent from "./components/Maincomponent";
 import SearchComponent from "./components/SearchComponent";
--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import Banner from "./common/BannerComponent";
 import Header from "./common/HeaderComponent";
+import { InputGroup, Button, FormControl } from "react-bootstrap";
 
 export default function SearchComponent() {
   return (
     <div className="App-search">
       <Header />
+      <div className="search-body">
+        <InputGroup className="mb-3">
+          <Button variant="outline-secondary" id="button-addon1">
+            검색
+          </Button>
+          <FormControl
+            aria-label="Example text with button addon"
+            aria-describedby="basic-addon1"
+            placeholder="검색어를 입력하세요."
+          />
+        </InputGroup>
+      </div>
       <Banner />
     </div>
   );
