c971d0888bf51732b0c34dacadec95b5f487550f
l0u0h0 <l0u0h0@gmail.com>
2022-07-10 23:19:56 +0900

    detail_update

--- src/components/crime/CrimedetailComponent.jsx
+++ src/components/crime/CrimedetailComponent.jsx
@@ -5,26 +5,10 @@ import ex_img from "../../image/camera.png";
 import { useLocation } from "react-router-dom";
 
 export default function Crimedetail() {
-  console.log(useLocation());
-  console.log(useLocation().search);
   const location = new URLSearchParams(useLocation().search);
-  console.log(location);
-  const type = location.get("category");
+  const category = location.get("category");
   const word = location.get("word");
   const mean = location.get("mean");
-  console.log(type);
-  const [category, setCategory] = useState();
-  useEffect(() => {
-    if (type === "gambling") {
-      setCategory("도박");
-    } else if (type === "voicefishing") {
-      setCategory("보이스 피싱");
-    } else if (type === "drug") {
-      setCategory("마약");
-    } else if (type === "gendercrime") {
-      setCategory("성범죄");
-    }
-  }, [type]);
   return (
     <div className="App-crimedetail">
       <Header />
@@ -33,7 +17,7 @@ export default function Crimedetail() {
         <hr className="title-body-between" />
         <div className="detail-body">
           <h3>카테고리 - {category}</h3>
-          {/* <img src={ex_img} alt="example_image" /> */}
+          <img src={ex_img} alt="example_image" />
           <p>{mean}</p>
         </div>
       </Card>
